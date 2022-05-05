96fca8c53c78322255bcfcdfb795090c63520962
wyq <acoffer@foxmail.com>
2022-05-05 18:11:47 +0800

    init

---
+++ Cookie-Health/Cookie-Health/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cookie-Health
+//
+//  Created by Acoffer on 2022/5/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.config()
+    }
+
+    private func config() {
+        view.backgroundColor = .black
+    }
+}
